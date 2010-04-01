2119f3a62b0462685af6ebf4ebcf02852cdf1687
hanoo <ythomann@etudiant.univ-mlv.fr>
2010-04-01 16:57:08 +0200

    print all :D

--- src/main.c
+++ src/main.c
@@ -5,9 +5,10 @@
 
 int main(int argc, char* argv[]){
     List *hash_table=init_hash_table();
-	parse_text("/tmp/test.txt", hash_table);
-	print_list_word(hash_table[0]);
+    	parse_text("./Colomba.txt", hash_table);
+    	print_list_word(hash_table[0]);
     free_hash(hash_table);
+
     return 0;
 }
 
