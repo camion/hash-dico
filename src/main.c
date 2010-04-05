7efa1c3ac95f5656962b4d82721dc4846ff02076
hanoo <ythomann@etudiant.univ-mlv.fr>
2010-04-05 16:03:44 +0200

    check if root execute

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <signal.h>
+
 #include "../include/read.h"
 #include "../include/util.h"
 #include "../include/word.h"
@@ -11,12 +13,16 @@ void quit(){
 
 int main(int argc, char* argv[]){
 
+
 /* execute quit() on ctrl-c */
     signal(SIGINT,quit);
+/* check if root execute the program */
+    if(getgid() == 0)fprintf(stderr,"You shouldn't run %s as root.\n",argv[0]);
 
     List *hash_table=init_hash_table();
-    	parse_text("./Colomba.txt", hash_table);
-    	print_list_word(hash_table[0]);
+
+    parse_text("./Colomba.txt", hash_table);
+    print_list_word(hash_table[0]);
     free_hash(hash_table);
 
     return 0;
