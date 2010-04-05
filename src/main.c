27decc33f92b232a11fe9889041afb30053ea5da
hanoo <ythomann@etudiant.univ-mlv.fr>
2010-04-05 15:58:15 +0200

    caught ctrl-c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
+#include <signal.h>
 #include "../include/read.h"
 #include "../include/util.h"
 #include "../include/word.h"
 
+void quit(){
+    printf("\nctrc-c caught, exiting...\n");
+    exit(1);
+}
+
 int main(int argc, char* argv[]){
+
+/* execute quit() on ctrl-c */
+    signal(SIGINT,quit);
+
     List *hash_table=init_hash_table();
     	parse_text("./Colomba.txt", hash_table);
     	print_list_word(hash_table[0]);
