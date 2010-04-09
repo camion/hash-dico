9d13479b9e9101315a9aed72c2e82bcc914b406e
hanoo <ythomann@etudiant.univ-mlv.fr>
2010-04-09 21:16:34 +0200

    little fix

--- src/main.c
+++ src/main.c
@@ -12,20 +12,20 @@ void quit(){
 }
 
 int main(int argc, char* argv[]){
+    List *hash_table=init_hash_table();
 
-
-/* execute quit() on ctrl-c */
+/* execute quit() on SIGINT (ctrl-c) */
     signal(SIGINT,quit);
 /* check if root execute the program */
     if(getgid() == 0)fprintf(stderr,"You shouldn't run %s as root.\n",argv[0]);
 
-    List *hash_table=init_hash_table();
+
 
     /* parse_text("./Colomba.txt", hash_table); */
     /* printf("%p\n",search_word(hash_table,"regrets")->value->positions); */
     /* print_list_word(hash_table[0]); */
     List w=NULL;
-    insert_lexico_word(&,"camion", (long)45);
+    insert_lexico_word(&w,"camion", (long)45);
     insert_lexico_word(&w,"camion", (long)55);
     insert_lexico_word(&w,"camzion", (long)55);
     print_list_pos(w->value->positions);
