856e19a75b60a7f95a8aedc8205f316749fb9d78
hanoo <ythomann@etudiant.univ-mlv.fr>
2010-04-11 00:23:01 +0200

    hash_progress_bar

--- src/read.c
+++ src/read.c
@@ -73,7 +73,7 @@ void parse_text(FILE* text, List *hash_table){
         key=hash_string(word)%HASH_SIZE;
     	insert_lexico_word(&(hash_table[key]), word, offset);
     }
-    progress_bar(1, 1);/* be sure to stay with 100% */
+    hash_progress_bar(1, 1);/* be sure to stay with 100% */
     printf("\n");
 }
 
